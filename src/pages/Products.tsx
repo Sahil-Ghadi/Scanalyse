a441996850c1193489f7708ccab3590c9822530c
Sahil-Ghadi <sahilghadi514@gmail.com>
2025-01-01 09:41:31 +0530

    fixed css

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -57,7 +57,7 @@ export default function Products() {
         </div>
       </div>
 
-      <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {filteredProducts.map(product => (
           <ProductCard key={product.id} product={product} />
         ))}
